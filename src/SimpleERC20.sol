6db726e1d7ce053134a1ccefe096774e35c78eb4
Anna Carroll <anna.s.carroll@gmail.com>
2024-08-15 16:27:34 +0100

    feat: simple ERC20 - Burnable + Ownable mint

---
+++ src/SimpleERC20.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.8.26;
+
+import {ERC20Burnable} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+/// @notice A contract deployed to Host chain that allows tokens to enter the rollup.
+contract SimpleERC20 is ERC20Burnable, Ownable {
+    constructor(address initialOwner, string memory name, string memory symbol)
+        Ownable(initialOwner)
+        ERC20(name, symbol)
+    {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
---
+++ test/SimpleERC20.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {SimpleERC20} from "../src/SimpleERC20.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract CounterTest is Test {
+    SimpleERC20 public test;
+
+    function setUp() public {
+        test = new SimpleERC20(address(this), "Test Token", "TEST");
+    }
+
+    function testMint() public {
+        test.mint(address(this), 100);
+        assertEq(test.balanceOf(address(this)), 100);
+    }
+
+    function testBurn() public {
+        test.mint(address(this), 100);
+        test.burn(100);
+        assertEq(test.balanceOf(address(this)), 0);
+    }
+}
